f9fa1f2a43195eb2123c77d99378ee6d661f7b12
fsmardani <42679276+fsmardani@users.noreply.github.com>
2021-08-11 01:03:53 +0430

    product model

--- GameStore/product/models.py
+++ GameStore/product/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Category (models.Model):
+    name = models.CharField(max_length=100)
+    cat_parent = models.ForeignKey(to='Category',on_delete=models.CASCADE)
+
+
+class Productbase(models.Model):
+    devices=(
+        ('ps4','ps4'),
+        ('ps5','ps5'),
+        ('all','all'),
+        ('xbox','xbox'),
+        ('nintendo', 'nintendo switch')
+    )
+    name = models.CharField(max_length=200)
+    stock = models.BooleanField(default=False)
+    device = models.CharField(max_length=20,choices=devices)
+    description = models.TextField(blank=True,null=True)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    price = models.FloatField(default=0.0)
+    added_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.name, self.description
+
+
